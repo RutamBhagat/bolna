2988dd4aaecddb27818e982b1bb8608ded1e4d63
Rutam Bhagat <rutambhagat@gmail.com>
2026-04-29 10:13:01 +0530

    chore: lint

--- packages/env/src/server.ts
+++ packages/env/src/server.ts
@@ -1,4 +1,5 @@
-/// <reference path="../env.d.ts" />
+import type {} from "../env";
+
 // For Cloudflare Workers, env is accessed via cloudflare:workers module
 // Types are defined in env.d.ts based on your alchemy.run.ts bindings
 export { env } from "cloudflare:workers";
